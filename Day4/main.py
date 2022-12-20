b5437feda8fe6355827b83c63091cd797ad417ac
Christian Payne <christianpayne522@gmail.com>
2022-12-19 19:55:06 -0800

    Day 4 Part 2 complete

--- Day4/main.py
+++ Day4/main.py
@@ -9,10 +9,19 @@ def fully_overlapped(a: list, b: list):
   len_set = len(set([*a, *b]))
   return len_set == len(a) or len_set == len(b)
 
+def any_overlap(a: list, b: list):
+  len_set = len(set([*a, *b]))
+  return len_set < len([*a, *b])
+
 pairs = [x.split(',') for x in data.split('\n')]
 
 def PartOne():
   overlapped_pairs = [pair for pair in pairs if fully_overlapped(getRange(pair[0]), getRange(pair[1]))]
   print('Part One | Overlapped Pairs:', len(overlapped_pairs))
 
+def PartTwo():
+  any_overlapped_pairs = [pair for pair in pairs if any_overlap(getRange(pair[0]), getRange(pair[1]))]
+  print('Part Two | Any Overlap Pairs:', len(any_overlapped_pairs))
+
 PartOne()
+PartTwo()
