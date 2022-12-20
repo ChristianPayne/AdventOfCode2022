d6f8f021825570e663a3bd7c042c0af58291b973
Christian Payne <christianpayne522@gmail.com>
2022-12-19 19:46:54 -0800

    Refactoring day 4 part 1

--- Day4/main.py
+++ Day4/main.py
@@ -1,25 +1,18 @@
 with open("input.txt", "r") as f:
  data = f.read()
 
-
 def getRange (elf_range: str):
   start, end = elf_range.split('-')
-  range_of_elf = list(range(int(start), int(end) + 1))
-  return range_of_elf
+  return list(range(int(start), int(end) + 1))
 
-def compare_ranges(a: list, b: list):
+def fully_overlapped(a: list, b: list):
   len_set = len(set([*a, *b]))
-
-  if( len_set == len(a) or len_set == len(b) ):
-    return True
-  else:
-    return False
-
+  return len_set == len(a) or len_set == len(b)
 
 pairs = [x.split(',') for x in data.split('\n')]
 
 def PartOne():
-  ranges = [compare_ranges(getRange(pair[0]), getRange(pair[1])) for pair in pairs]
-  print('Part One:', len([x for x in ranges if x == True]))
+  overlapped_pairs = [pair for pair in pairs if fully_overlapped(getRange(pair[0]), getRange(pair[1]))]
+  print('Part One | Overlapped Pairs:', len(overlapped_pairs))
 
 PartOne()
